e8e1848d70dfe4879a7ac89cc1ada5191f9a3fff
RushilShandilya <rushilcujoh@gmail.com>
2024-01-14 18:45:01 +0530

    Commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,14 +1,22 @@
 package com.driver;
 
 public class Pizza {
-
     private int price;
     private Boolean isVeg;
     private String bill;
+    private final int[] priceArray = new int[4];
+    private int cheeseCounter = 0 , toppingCounter = 0;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        //PriceArray[0] is Base Price
+        if(isVeg){
+            priceArray[0] = 300;
+            price=300;
+        }if(!isVeg){
+            priceArray[0] = 400;
+            price=400;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +24,51 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        //PriceArray[1] is ExtraCheese
+        price+=80;
+        cheeseCounter++;
+        priceArray[1] = 80*cheeseCounter;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        //PriceArray[2] is ExtraToppings
+        toppingCounter++;
+        if(isVeg){
+            price+=70;
+            priceArray[2] +=70*toppingCounter;
+        }
+        if(!isVeg){
+            price+=120;
+            priceArray[2] += 120*toppingCounter;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        //PriceArray[3] is Paper Bag
+        priceArray[3] = 20;
+        price+=20;
     }
 
     public String getBill(){
-        // your code goes here
+        for(int i = 0 ; i<priceArray.length ; ++i){
+            switch (i) {
+                default:
+                    break;
+                case 0:
+                    if (priceArray[i] != 0) bill = "Base Price Of The Pizza: " + priceArray[0] + "\n";
+                    break;
+                case 1:
+                    if(priceArray[i] != 0) bill += "Extra Cheese Added: " + priceArray[1] + "\n";
+                    break;
+                case 2:
+                    if(priceArray[i] != 0) bill += "Extra Toppings Added: " + priceArray[2] + "\n";
+                    break;
+                case 3:
+                    if(priceArray[i] != 0)bill += "Paperbag Added: " + priceArray[3] + "\n";
+                    break;
+            }
+        }
+        bill+= "Total Price: " + price + "\n";
         return this.bill;
     }
 }
